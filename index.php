941d6f06b07ddadb488722d11f1f82d1dd172ef4
adhimeong <adhimeong@gmail.com>
2017-01-24 12:10:29 +0700

    tambah p di index

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 </head>
 <body>
 	<h1>Ini halaman index</h1>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae quibusdam quaerat odio doloribus. Unde placeat voluptates repudiandae animi a vero nisi voluptatem fuga eum, ex illo! Dolorem reiciendis mollitia ad.</p>
 	<p>hello git</p>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus provident eos vitae ad praesentium earum soluta quis architecto ipsum voluptatum sint, velit inventore ratione vero quaerat maxime nobis debitis consequuntur!</p>
 </body>
 </html>
